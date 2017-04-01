18c227f0c65e4bd1d75d05c89a0b2c2ac1d899b4
Abhigyan Singh <abhigyan2311@gmail.com>
2017-04-01 17:30:18 +0530

    Update

--- DBSKYC/RegistrationViewController.swift
+++ DBSKYC/RegistrationViewController.swift
@@ -59,50 +59,30 @@ class RegistrationViewController: UIViewController, UIImagePickerControllerDeleg
                 }
             })
         }
-        print(2221212)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         imagePicker.dismiss(animated: true, completion: nil)
-        camView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
-        //this block of code grabs the path of the file
+//        camView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+
         let nsDocumentDirectory = FileManager.SearchPathDirectory.documentDirectory
         let nsUserDomainMask    = FileManager.SearchPathDomainMask.userDomainMask
         let paths               = NSSearchPathForDirectoriesInDomains(nsDocumentDirectory,nsUserDomainMask, true)
         if let dirPath = paths.first
         {
-            let imageURL = URL(fileURLWithPath: dirPath).appendingPathComponent("Image2.png")
-            let image    = UIImage(contentsOfFile: imageURL.path)
-            print(imageURL.description)
+            photoURL = URL(fileURLWithPath: dirPath).appendingPathComponent("Image2.png")
+            let image    = UIImage(contentsOfFile: photoURL.path)
+            camView.image = image
+            
             // Do whatever you want with the image
         }
-//        let imageURL = info[UIImagePickerControllerReferenceURL] as! NSURL
-//        let imagePath =  imageURL.path!
-//        let localPath = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(imagePath)
-//        
-//        //this block of code adds data to the above path
-//        let path = localPath?.relativePath
-//        let imageName = info[UIImagePickerControllerOriginalImage] as! UIImage
-//        let data = UIImagePNGRepresentation(imageName)
-//        let fileUrl = NSURL(string: imagePath)
-//        do {
-//            try data?.write(to: fileUrl as! URL, options: .noFileProtection)
-//            print("Written to disk")
-//        }
-//        catch let error as NSError {
-//            print(error.description)
-//        }
-//        
-//        //this block grabs the NSURL so you can use it in CKASSET
-//        photoURL = NSURL(fileURLWithPath: path!) as URL
-//        print(photoURL)
     }
     
     @IBAction func uploadS3(_ sender: Any) {
         let expression = AWSS3TransferUtilityUploadExpression()
         expression.progressBlock = {(task, progress) in DispatchQueue.main.async(execute: {
             // Do something e.g. Update a progress bar.
-        })
+            })
         }
         self.completionHandler = { (task, error) -> Void in
             DispatchQueue.main.async(execute: {
@@ -123,7 +103,6 @@ class RegistrationViewController: UIViewController, UIImagePickerControllerDeleg
                                     if let _ = task.result {
                                         // Do something with uploadTask.
                                     }
-                                    
                                     return nil;
         }
     }
